84bd29412146126e49bc307de5eeb7f558d3699a
Jesus Alcantara <jesus.alcantara@alten.es>
2023-02-16 17:20:31 +0100

    create components

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>¿Quién es este pokémon?</h1>
+    <pokemon-picture />
+    <pokemon-options />
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
+
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    }
+
+}
+</script>
